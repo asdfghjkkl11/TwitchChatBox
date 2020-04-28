43ca015f5ed871c57cf6cebfd7ecb065fdba63ba
asdfghjkkl11 <asdfghjkkl11@naver.com>
2020-04-28 18:30:55 +0900

    make basement

--- JsonLoader.cs
+++ JsonLoader.cs
@@ -13,30 +13,32 @@ namespace TwitchChatBox
     {
         private string strLocalPath = System.Environment.CurrentDirectory;
         public string Address { get; set; }
-        public string Ports { get; set; }
+        public int Ports { get; set; }
         public string Password { get; set; }
         public string Nickname { get; set; }
         public string Channel { get; set; }
-
-        public void GetData()
+        public JsonLoader()
         {
-            if (!System.IO.File.Exists(strLocalPath + "\\info.json")) {
+            if (!System.IO.File.Exists(strLocalPath + "\\info.json"))
+            {
                 //파일없음
                 return;
             }
-            string strReturnValue = System.IO.File.ReadAllText("info.json"); 
-            if (strReturnValue == "") { 
+            string strReturnValue = System.IO.File.ReadAllText("info.json");
+            if (strReturnValue == "")
+            {
                 //불러오기 실패
-                return; 
+                return;
             }
             JsonTextParser jtr = new JsonTextParser();
             JsonObject jo = jtr.Parse(strReturnValue);
             JsonObjectCollection jac = (JsonObjectCollection)jo;
             Address = jac["Address"].GetValue().ToString();
-            Ports = jac["Ports"].GetValue().ToString();
+            Ports = int.Parse(jac["Ports"].GetValue().ToString());
             Password = jac["Password"].GetValue().ToString();
             Nickname = jac["Nickname"].GetValue().ToString();
             Channel = jac["Channel"].GetValue().ToString();
+            
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,33 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TwitchChatBox
 {
     public partial class MainWindow : Window
     {
         JsonLoader jsonlaoder = new JsonLoader();
-        
         public MainWindow()
         {
             InitializeComponent();
-            jsonlaoder.GetData();
-            tbSettingText.Text = jsonlaoder.Address + '\n';
-            tbSettingText.Text += jsonlaoder.Ports + '\n';
-            tbSettingText.Text += jsonlaoder.Nickname + '\n';
-            tbSettingText.Text += jsonlaoder.Password + '\n';
-            tbSettingText.Text += jsonlaoder.Channel + '\n';
+            
+            System.Threading.Thread thread1 = new System.Threading.Thread(
+            new System.Threading.ThreadStart(
+                delegate ()
+                {
+                    IRCClient irc = new IRCClient(jsonlaoder.Address, jsonlaoder.Ports,
+                            jsonlaoder.Nickname, jsonlaoder.Password, jsonlaoder.Channel);
+                    PingSender ping = new PingSender(irc);
+                    ping.Start();
+                    while (true)
+                    {
+                        // Read any message from the chat room
+                        string message = irc.ReadMessage();
+                        Console.WriteLine(message);
+                        // Print raw irc messages
+                        System.Windows.Threading.DispatcherOperation
+                            dispatcherOp = tbSettingText.Dispatcher.BeginInvoke(
+                            System.Windows.Threading.DispatcherPriority.Normal,
+                            new Action(
+                            delegate ()
+                            {
+                                tbSettingText.Text = message;
+                            }
+                        ));
+                        if (message.Contains("PRIVMSG"))
+                        {
+                            // Messages from the users will look something like this (without quotes):
+                            // Format: ":[user]![user]@[user].tmi.twitch.tv PRIVMSG #[channel] :[message]"
+
+                            // Modify message to only retrieve user and message
+                            int intIndexParseSign = message.IndexOf('!');
+                            string userName = message.Substring(1, intIndexParseSign - 1); // parse username from specific section (without quotes)
+                                                                                           // Format: ":[user]!"
+                                                                                           // Get user's message
+                            intIndexParseSign = message.IndexOf(" :");
+                            message = message.Substring(intIndexParseSign + 2);
+
+                            //Console.WriteLine(message); // Print parsed irc message (debugging only)
+
+                        }
+                    }
+                }
+            ));
+            thread1.Start();
         }
     }
 }
